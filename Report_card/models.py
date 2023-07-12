65d0096346add697305fd9c126d2cd31354d31c3
LeandroLino <leao.lino7@gmail.com>
2023-07-12 03:08:25 -0300

    Fix :: Tried fix 'null has no effect on ManyToManyField' in Railway

--- Report_card/models.py
+++ Report_card/models.py
@@ -4,6 +4,6 @@ class ReportCardsModel(models.Model):
     id = models.AutoField(primary_key=True)
     delivery_date = models.DateTimeField()
     student = models.ForeignKey('Students.StudentsModel', on_delete=models.CASCADE, null=True)
-    notes = models.ManyToManyField('Report_notes.ReportNotesModel', null=True)
+    notes = models.ManyToManyField('Report_notes.ReportNotesModel', blank=True)
 
 
